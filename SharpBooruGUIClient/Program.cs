9db91fae21bc1b3fc7ccffda343f04ab47957618
teamalpha5441 <teamalpha5441@gmail.com>
2013-07-31 17:31:25 +0200

    Added login prompt to program start

--- SharpBooruGUIClient/Program.cs
+++ SharpBooruGUIClient/Program.cs
@@ -6,13 +6,24 @@ namespace TA.SharpBooru.Client.GUI
     public static class Program
     {
         [STAThread]
-        public static void Main()
+        public static int Main(string[] args)
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
 
-            using (Booru booru = new Booru("localhost", 2400, "guest", "guest")) //TODO Client config
-                Application.Run(new MainForm(booru));
+            string server = args.Length > 0 ? args[0] : "127.0.01";
+
+            using (LoginDialog lDialog  =new LoginDialog())
+                if (lDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string username = lDialog.Username;
+                    string password = lDialog.Password;
+
+                    using (Booru booru = new Booru(server, 2400, username, password))
+                        Application.Run(new MainForm(booru));
+                }
+
+            return 0;
         }
     }
 }
