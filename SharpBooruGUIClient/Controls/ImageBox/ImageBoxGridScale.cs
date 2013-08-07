523a5c5df74a8e8d81eddbe452baf633ec6ba354
teamalpha5441 <teamalpha5441@gmail.com>
2013-08-07 17:14:06 +0200

    Added ImageBox control

---
+++ SharpBooruGUIClient/Controls/ImageBox/ImageBoxGridScale.cs
@@ -0,0 +1,28 @@
+﻿namespace Cyotek.Windows.Forms
+{
+  /// <summary>
+  ///   Specifies the size of the background texture grid.
+  /// </summary>
+  public enum ImageBoxGridScale
+  {
+    /// <summary>
+    ///   Half of size.
+    /// </summary>
+    Tiny,
+
+    /// <summary>
+    ///   Default size.
+    /// </summary>
+    Small,
+
+    /// <summary>
+    ///   50% increase of default size.
+    /// </summary>
+    Medium,
+
+    /// <summary>
+    ///   100% increase of default size.
+    /// </summary>
+    Large
+  }
+}
