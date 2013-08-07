1f8b50417e9b79916b0fbee758fe8cf9ab065083
teamalpha5441 <teamalpha5441@gmail.com>
2013-08-07 17:17:31 +0200

    BugFix: Admin button Anchor wrong fixed

--- SharpBooruGUIClient/MainForm.Designer.cs
+++ SharpBooruGUIClient/MainForm.Designer.cs
@@ -31,14 +31,14 @@
             this.components = new System.ComponentModel.Container();
             this.labelSearchBox = new System.Windows.Forms.Label();
             this.buttonImportDialog = new System.Windows.Forms.Button();
-            this.searchBox = new TA.SharpBooru.Client.GUI.Controls.TagTextBox();
-            this.booruThumbView = new TA.SharpBooru.Client.GUI.Controls.BooruThumbView();
             this.buttonChangeUser = new System.Windows.Forms.Button();
             this.buttonRefresh = new System.Windows.Forms.Button();
             this.buttonAdminTools = new System.Windows.Forms.Button();
             this.adminContextMenuStrip = new System.Windows.Forms.ContextMenuStrip(this.components);
             this.saveBooruToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.killServerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.searchBox = new TA.SharpBooru.Client.GUI.Controls.TagTextBox();
+            this.booruThumbView = new TA.SharpBooru.Client.GUI.Controls.BooruThumbView();
             this.adminContextMenuStrip.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -61,27 +61,6 @@
             this.buttonImportDialog.Text = "Import";
             this.buttonImportDialog.UseVisualStyleBackColor = true;
             this.buttonImportDialog.Click += new System.EventHandler(this.buttonImportDialog_Click);
-            // 
-            // searchBox
-            // 
-            this.searchBox.Location = new System.Drawing.Point(12, 31);
-            this.searchBox.Name = "searchBox";
-            this.searchBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.searchBox.Size = new System.Drawing.Size(100, 20);
-            this.searchBox.TabIndex = 0;
-            // 
-            // booruThumbView
-            // 
-            this.booruThumbView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
-                        | System.Windows.Forms.AnchorStyles.Left)
-                        | System.Windows.Forms.AnchorStyles.Right)));
-            this.booruThumbView.BackColor = System.Drawing.Color.SteelBlue;
-            this.booruThumbView.LabelForeColor = System.Drawing.Color.White;
-            this.booruThumbView.Location = new System.Drawing.Point(118, 12);
-            this.booruThumbView.Name = "booruThumbView";
-            this.booruThumbView.Size = new System.Drawing.Size(459, 323);
-            this.booruThumbView.TabIndex = 4;
-            this.booruThumbView.ThumbViewBackColor = System.Drawing.Color.White;
             // 
             // buttonChangeUser
             // 
@@ -107,6 +86,7 @@
             // 
             // buttonAdminTools
             // 
+            this.buttonAdminTools.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.buttonAdminTools.Location = new System.Drawing.Point(12, 225);
             this.buttonAdminTools.Name = "buttonAdminTools";
             this.buttonAdminTools.Size = new System.Drawing.Size(100, 23);
@@ -133,6 +113,27 @@
             this.killServerToolStripMenuItem.Name = "killServerToolStripMenuItem";
             this.killServerToolStripMenuItem.Size = new System.Drawing.Size(133, 22);
             this.killServerToolStripMenuItem.Text = "Kill Server";
+            // 
+            // searchBox
+            // 
+            this.searchBox.Location = new System.Drawing.Point(12, 31);
+            this.searchBox.Name = "searchBox";
+            this.searchBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.searchBox.Size = new System.Drawing.Size(100, 20);
+            this.searchBox.TabIndex = 0;
+            // 
+            // booruThumbView
+            // 
+            this.booruThumbView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.booruThumbView.BackColor = System.Drawing.Color.SteelBlue;
+            this.booruThumbView.LabelForeColor = System.Drawing.Color.White;
+            this.booruThumbView.Location = new System.Drawing.Point(118, 12);
+            this.booruThumbView.Name = "booruThumbView";
+            this.booruThumbView.Size = new System.Drawing.Size(459, 323);
+            this.booruThumbView.TabIndex = 4;
+            this.booruThumbView.ThumbViewBackColor = System.Drawing.Color.White;
             // 
             // MainForm
             // 
