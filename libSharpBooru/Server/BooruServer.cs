dd6541773fe4f4d83c501f90f66e1870d48f7efe
teamalpha5441 <teamalpha5441@gmail.com>
2013-08-25 19:56:28 +0200

    Updated the new Server classes

---
+++ libSharpBooru/Server/BooruServer.cs
@@ -0,0 +1,47 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Security.Cryptography.X509Certificates;
+
+namespace TA.SharpBooru.Server.Experimental
+{
+    public class BooruServer : Server
+    {
+        private TcpListener _Listener;
+        private X509Certificate _Certificate;
+        private Booru _Booru;
+        private Logger _Logger;
+
+        public static readonly uint ServerVersion = 2;
+        public override string ServerName { get { return "BooruServer"; } }
+
+        public Booru Booru { get { return _Booru; } }
+        public Logger Logger { get { return _Logger; } }
+        public X509Certificate Certificate { get { return _Certificate; } }
+
+        public BooruServer(Booru Booru, Logger Logger, X509Certificate Certificate, ushort Port = 2400)
+        {
+            _Booru = Booru;
+            _Logger = Logger;
+            _Certificate = Certificate;
+            _Listener = new TcpListener(IPAddress.Any, Port);
+        }
+
+        public override object ConnectClient() { return _Listener.AcceptTcpClient(); }
+
+        public override void HandleClient(object Client)
+        {
+            TcpClient client = Client as TcpClient;
+            //Handle Client here
+        }
+
+        public override bool HandleException(System.Exception Ex)
+        {
+            _Logger.LogException(ServerName, Ex);
+            return true;
+        }
+
+        public override void StartListener() { _Listener.Start(); }
+
+        public override void StopListener() { _Listener.Stop(); }
+    }
+}
--- libSharpBooru/Server/Server.cs
+++ libSharpBooru/Server/Server.cs
@@ -1,12 +1,83 @@
-﻿namespace TA.SharpBooru.Server
-{
-    public class Server
-    {
-        public abstract string ServerName { get; }
+﻿using System;
+using System.Threading;
+using Amib.Threading;
 
-        public abstract void Start();
-        public abstract void Stop();
-        public abstract object GetClient();
+namespace TA.SharpBooru.Server
+{
+    public abstract class Server
+    {
+        private SmartThreadPool _ThreadPool = new SmartThreadPool();
+        protected Thread _ConnectClientThread = null;
+        protected bool _ConnectClientThreadRunning = false;
+        protected object _StartStopLock = new object();
+
+        public abstract string ServerName { get; }
+        public SmartThreadPool ThreadPool { get { return _ThreadPool; } }
+
+        public abstract object ConnectClient();
         public abstract void HandleClient(object Client);
+        public abstract bool HandleException(Exception Ex);
+        public abstract void StartListener();
+        public abstract void StopListener();
+
+        public void Start()
+        {
+            lock (_StartStopLock)
+                if (!_ConnectClientThreadRunning)
+                {
+                    _ConnectClientThread = new Thread(_HandlerStage1);
+                    _ConnectClientThreadRunning = true;
+                    StartListener();
+                    _ConnectClientThread.Start();
+                }
+                else throw new Exception("Already running");
+        }
+
+        public void Stop()
+        {
+            lock (_StartStopLock)
+                if (_ConnectClientThreadRunning)
+                {
+                    _ConnectClientThreadRunning = false;
+                    StopListener();
+                    _ThreadPool.WaitForIdle(10 * 1000); //Wait max. 9s for idle
+                    _ThreadPool.Cancel(true);
+                    _ConnectClientThread.Abort();
+                }
+                else throw new Exception("Not running");
+        }
+
+        protected object _HandlerStage2(object Client)
+        {
+            try { HandleClient(Client); }
+            catch (Exception ex)
+            {
+                if (!_ConnectClientThreadRunning)
+                    return null;
+                else if (!HandleException(ex))
+                    throw ex;
+            }
+            return null;
+        }
+
+        protected void _HandlerStage1()
+        {
+            while (_ConnectClientThreadRunning)
+            {
+                try
+                {
+                    object client = ConnectClient();
+                    if (_ConnectClientThreadRunning)
+                        _ThreadPool.QueueWorkItem(new WorkItemCallback(_HandlerStage2), client);
+                }
+                catch (Exception ex)
+                {
+                    if (!_ConnectClientThreadRunning)
+                        break; //Exit the thread silently
+                    else if (!HandleException(ex))
+                        throw ex;
+                }
+            }
+        }
     }
 }
