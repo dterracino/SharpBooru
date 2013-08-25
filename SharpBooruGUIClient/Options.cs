d870060321719414929c7b9011f0c9f466b51c23
teamalpha5441 <teamalpha5441@gmail.com>
2013-08-26 01:16:57 +0200

    Added Console option

--- SharpBooruGUIClient/Options.cs
+++ SharpBooruGUIClient/Options.cs
@@ -8,11 +8,14 @@ namespace TA.SharpBooru.Client.GUI
         [Option('s', "server", Required = true, HelpText = "Server IP or hostname + port")]
         public string Server { get; set; }
 
-        [Option('u', "username", Required = false, HelpText = "The username for auto login")]
+        [Option('u', "username", Required = false, DefaultValue = "guest", HelpText = "The username for auto login")]
         public string Username { get; set; }
 
-        [Option('p', "password", Required = false, HelpText = "The password for auto login")]
+        [Option('p', "password", Required = false, DefaultValue = "guest", HelpText = "The password for auto login")]
         public string Password { get; set; }
+
+        [Option('c', "console", Required = false, DefaultValue = false, HelpText = "Use the console mode (no GUI)")]
+        public bool Console { get; set; }
 
         [HelpOption('h', "help")]
         public string GetUsage() { return HelpText.AutoBuild(this); }
