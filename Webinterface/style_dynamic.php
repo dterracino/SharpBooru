0bc939e854f71a256b0cb302f0b0abb191e5d0d6
teamalpha5441 <teamalpha5441@gmail.com>
2014-10-14 11:11:09 +0200

    Small changes

--- Webinterface/style_dynamic.php
+++ Webinterface/style_dynamic.php
@@ -9,16 +9,16 @@ cache_headers(12 * 3600);
 ?>
 
 div.thumb img {
-    max-width: <?php echo $thumb_size; ?>px;
-    max-height: <?php echo $thumb_size; ?>px;
-    vertical-align: middle;
+	max-width: <?php echo $thumb_size; ?>px;
+	max-height: <?php echo $thumb_size; ?>px;
+	vertical-align: middle;
 }
 
 div.thumb {
-    width: <?php echo $thumb_size; ?>px;
-    height: <?php echo $thumb_size; ?>px;
-    float: left;
-    margin: 6px;
-    line-height: <?php echo $thumb_size; ?>px;
-    text-align: center;
+	width: <?php echo $thumb_size; ?>px;
+	height: <?php echo $thumb_size; ?>px;
+	float: left;
+	margin: 6px;
+	line-height: <?php echo $thumb_size; ?>px;
+	text-align: center;
 }
