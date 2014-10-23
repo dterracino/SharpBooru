d0c97b371f81bce0bba9dfc3b574b0a6d0df7a73
teamalpha5441 <teamalpha5441@gmail.com>
2014-10-23 11:48:12 +0200

    Renamed image files

--- Webinterface/index.php
+++ Webinterface/index.php
@@ -14,13 +14,17 @@ $div_width = 68 * ($counter_len + 1);
 echo '<div style="width: ' . $div_width . 'px; margin-left: auto; margin-right: auto; margin-top: 180px; text-align: center;">';
 
 for ($i = 0; $i < $counter_len; $i++)
-	echo '<img alt="' . $counter[$i] . '" src="counter/' . $counter[$i] . '.png">';
+	echo '<img alt="' . $counter[$i] . '" src="images/counter/' . $counter[$i] . '.png">';
 
 echo "<br><br>";
 
 echo '<form action="search.php" method="GET">';
 echo '<input style="width: 100%;" class="tagbox" type="text" name="tags">';
 echo "</form>";
+
+echo '<span style="font-size: 10px;">';
+echo "Running SharpBooru by Eggy";
+echo "</span>";
 
 echo "</div>";
 html_footer();
