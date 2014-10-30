9cf2dc4dc8d8a2b769efe075befafbc290d16b4c
teamalpha5441 <teamalpha5441@gmail.com>
2014-10-30 23:40:17 +0100

    Implemented MailNotificator

---
+++ Server/MailNotificator.cs
@@ -0,0 +1,81 @@
+using System;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Net.Mail;
+using System.Collections.Generic;
+
+namespace TA.SharpBooru
+{
+    public class MailNotificator
+    {
+        private string _Host;
+        private ushort _Port;
+        private NetworkCredential _Cred;
+
+        private MailAddress _From = null;
+        private MailAddress _To = null;
+
+        public MailNotificator(string Host, ushort Port, string Username, string Password, MailAddress From, MailAddress To)
+        {
+            _Host = Host;
+            _Port = Port;
+            _Cred = new NetworkCredential(Username, Password);
+
+            _From = From;
+            _To = To;
+        }
+
+        private SmtpClient ConnectClient()
+        {
+            SmtpClient client = new SmtpClient(_Host, _Port);
+            client.EnableSsl = true;
+            client.DeliveryMethod = SmtpDeliveryMethod.Network;
+            client.UseDefaultCredentials = false;
+            client.Credentials = _Cred;
+            return client;
+        }
+
+        public bool SendMail(string Subject, string Body)
+        {
+            List<Attachment> tmpList = null;
+            return SendMail(Subject, Body, tmpList);
+        }
+
+        public bool SendMail(string Subject, string Body, Attachment Attachment)
+        {
+            var tmpList = new List<Attachment>(1);
+            tmpList.Add(Attachment);
+            return SendMail(Subject, Body, tmpList);
+        }
+
+        public bool SendMail(string Subject, string Body, List<Attachment> Attachments)
+        {
+            using (var client = ConnectClient())
+            using (var mail = new MailMessage())
+                try
+                {
+                    mail.From = _From;
+                    mail.To.Add(_To);
+                    mail.Subject = Subject;
+                    mail.Body = Body;
+                    if (Attachments != null)
+                        foreach (var att in Attachments)
+                            mail.Attachments.Add(att);
+                    client.Send(mail);
+                    return true;
+                }
+                catch { return false; }
+        }
+
+        public bool NotificatePostAdded(ulong ID, BooruPost Post, BooruImage Thumb)
+        {
+            StringBuilder mailBody = new StringBuilder();
+            mailBody.AppendLine("User " + Post.User + " added post " + ID);
+            mailBody.AppendLine("https://eggy.hopto.org/booru/post.php?id=" + ID);
+            using (var stream = new MemoryStream(Thumb.Bytes))
+            using (var att = new Attachment(stream, "thumb.jpg", "image/jpeg"))
+                return SendMail("Post " + ID + " added", mailBody.ToString(), att);
+        }
+    }
+}
